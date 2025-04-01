19dde834722842007560630f43e75353466f07f1
ncolle3 <nick.lopes1000@gmail.com>
2025-04-01 19:12:57 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function mostrarMensagem() {
+    const mensagem = document.getElementById('mensagem');
+    mensagem.innerHTML = "A educação transforma vidas, permitindo o acesso a novos conhecimentos, habilidades e oportunidades. Ela é a base para a mudança e o progresso social!";
+    mensagem.style.color = "#4CAF50";
+    mensagem.style.fontSize = "1.2em";
+    mensagem.style.fontWeight = "bold";
+}
